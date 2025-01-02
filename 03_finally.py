c21353f8e9d45f8b491a6d1d415f605557ffadec
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-03 01:22:58 +0530

    finally

---
+++ 03_finally.py
@@ -0,0 +1,13 @@
+
+def func():
+    try:
+        a=int(input("a= "))
+        # b=int(input("b= "))
+        print(a)
+    except Exception as e:
+        print(e)
+    finally:
+        print("thank you")  # works in both case
+     
+func()  
+print("End of the program")  # this also works in both case
